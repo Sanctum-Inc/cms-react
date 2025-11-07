068e3a8f3e0cd8845260dca18888e6e16d77a27a
Tshego Motlatle <tshegomotlatle87@gmail.com>
2025-11-07 17:02:05 +0200

    💄 fix: button definitions

---
+++ src/Components/Buttons/PrimaryButton.tsx
@@ -0,0 +1,30 @@
+const colorMap = {
+    red: 'bg-red-500 hover:bg-red-700',
+    blue: 'bg-blue-500 hover:bg-blue-700',
+    green: 'bg-green-500 hover:bg-green-700',
+    gray: 'bg-gray-500 hover:bg-gray-700',
+};
+
+
+interface PrimaryButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    color?: keyof typeof colorMap;
+    hoverColor?: string | undefined
+    centerText?: boolean;
+}
+
+const PrimaryButton = ({ color = 'blue', centerText = true, ...props }: PrimaryButtonProps) => {
+    const base =
+        'w-full text-white font-bold py-2 px-4 rounded-xl transition duration-300';
+    const alignment = centerText ? 'text-center' : '';
+    const colorClasses = colorMap[color];
+
+    return (
+        <button
+            className={`${base} ${colorClasses} ${alignment}`}
+            type='button'
+            {...props}
+        />
+    );
+}
+
+export default PrimaryButton;
