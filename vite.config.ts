ec8a2c18d402afd6b14d6314396c61c1a689dde7
Tshegofatsho <tshegomotlatle87@gmail.com>
2026-02-09 13:54:09 +0200

    🙈update gitignore and commit .env

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
+
+
+const basename = import.meta.env.VITE_ROUTER_BASENAME || "/";
+
 export default defineConfig({
-  base: "/cms-react/",
+  base: basename,
   plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
