66badc8db70edd5c97032a7c9e71eb5ca89570d6
Tshegofatsho <tshegomotlatle87@gmail.com>
2026-02-10 08:11:39 +0200

    🐛 fix missing slash on vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
-  base: process.env.VITE_ROUTER_BASENAME ?? "/",
+  base: process.env.VITE_ROUTER_BASENAME
+    ? `${process.env.VITE_ROUTER_BASENAME}/`
+    : "/",
   plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
